0f6469d7325e74f922179475193705a175e169a6
Sinco <113909588+sphis-Sinco@users.noreply.github.com>
2025-01-16 19:39:16 +0000

    this could work!!!!!

---
+++ source/jsons.js
@@ -0,0 +1,10 @@
+export const JSONS = [
+    {
+        "title":"Sonic the Hedgehog 4",
+        "type": "MOVIE",
+        "release_date":"Summer 2025",
+        "place":"Unknown",
+        "speculation": false,
+        "png_file_location":"sonic 4.jpeg"
+    }
+]
