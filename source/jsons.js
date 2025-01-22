5d16712a12b8bc47e584a03590159108a4d6ffe6
Sinco <113909588+sphis-Sinco@users.noreply.github.com>
2025-01-22 17:30:07 +0000

    make it pretty and add sum other stuff

--- source/jsons.js
+++ source/jsons.js
@@ -5,7 +5,8 @@ export const JSONS = [
         "release_date":"March 19th 2027",
         "place":"Unknown location",
         "speculation": false,
-        "png_file_location":"sonic 4.jpeg"
+        "png_file_location":"sonic 4.jpeg",
+        "source": "https://variety.com/2025/film/news/sonic-4-release-date-march-2027-1236280697/#:~:text=%E2%80%9CSonic%20the%20Hedgehog%204%E2%80%9D%20is,released%20on%20March%2019%2C%202027."
     },
     {
         "title":"The Super Mario Bros. Movie 2",
@@ -21,6 +22,7 @@ export const JSONS = [
         "release_date":"January 31st 2025",
         "place":"Unknown location",
         "speculation": false,
-        "png_file_location":"dogman.jpeg"
+        "png_file_location":"dogman.jpeg",
+        "source": "https://en.wikipedia.org/wiki/Dog_Man_%28film%29"
     }
 ]
--- source/main.js
+++ source/main.js
@@ -7,22 +7,79 @@ var showsbody = document.getElementById("shows");
 
 var i = 0
 for (i in thejson) {
-    var p = document.createElement("p");
-    var img = document.createElement("img");
 
     var ismovie = thejson[i].type.toLowerCase() == 'movie';
-    
-    p.innerHTML = thejson[i].title + ' - ' + thejson[i].release_date + ' ('+(thejson[i].speculation == true ? 'SPECULATED' : 'OFFICIAL')+' RELEASE IN '+thejson[i].place.toUpperCase()+')';
-    img.src = 'assets/pngs/'+(ismovie ? 'movies' : 'shows')+'/'+thejson[i].png_file_location
-    img.width = 100;
+
+    var divcard = document.createElement('div');
+    divcard.className = 'card';
+    divcard.style = "width: 18rem;";
+    var divcardimg = document.createElement('img');
+    divcardimg.src = 'assets/pngs/'+(ismovie ? 'movies' : 'shows')+'/'+thejson[i].png_file_location;
+    divcardimg.className = "card-img-top"
+    divcard.appendChild(divcardimg);
+    var divcarddiv = document.createElement('div');
+    divcarddiv.class = "card-body";
+    var carddiveheader = document.createElement('h5');
+    carddiveheader.class = "card-title";
+    carddiveheader.innerHTML = thejson[i].title;
+    divcarddiv.appendChild(carddiveheader);
+    var carddivpara = document.createElement('p');
+    carddivpara.className = "card-text";
+    carddivpara.innerHTML = thejson[i].release_date + ' ('+(thejson[i].speculation == true ? 'SPECULATED' : 'OFFICIAL')+' RELEASE IN '+thejson[i].place.toUpperCase()+')';
+    divcarddiv.appendChild(carddivpara);
+    var carddivlink = document.createElement('a');
+    carddivlink.href = thejson[i].source;
+    carddivlink.class = "btn btn-primary";
+    carddivlink.innerHTML = "Source";
+    if (thejson[i].source != null) divcarddiv.appendChild(carddivlink);
+    divcard.appendChild(divcarddiv);
+
 
     if (ismovie) {
-        moviesbody.appendChild(p);
-        moviesbody.appendChild(img);
+        moviesbody.appendChild(divcard);
+        moviesbody.appendChild(document.createElement('br'));
     } else {
-        showsbody.appendChild(p);
-        showsbody.appendChild(img);
+        showsbody.appendChild(divcard);
+        showsbody.appendChild(document.createElement('br'));
     }
 
     i++;
 }
+
+/**
+ * <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+ */
+
+/*
+var divcard = document.createElement('div');
+divcard.className = 'card';
+divcard.style = "width: 18rem;";
+var divcardimg = document.createElement('img');
+divcardimg.src = 'assets/pngs/movies/sonic 4.jpeg';
+divcardimg.className = "card-img-top"
+divcard.appendChild(divcardimg);
+var divcarddiv = document.createElement('div');
+divcarddiv.class = "card-body";
+var carddiveheader = document.createElement('h5');
+carddiveheader.class = "card-title";
+carddiveheader.innerHTML = "Card title";
+divcarddiv.appendChild(carddiveheader);
+var carddivpara = document.createElement('p');
+carddivpara.className = "card-text";
+carddivpara.innerHTML = "Lorem";
+divcarddiv.appendChild(carddivpara);
+var carddivlink = document.createElement('a');
+carddivlink.href = "#";
+carddivlink.class = "btn btn-primary";
+carddivlink.innerHTML = "Source";
+divcarddiv.appendChild(carddivlink);
+divcard.appendChild(divcarddiv);
+document.body.append(divcard);
+*/
