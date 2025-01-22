ce4fcc1f0209c43936f3131a7ee3f4cf495cc088
Sinco <113909588+sphis-Sinco@users.noreply.github.com>
2025-01-22 16:33:28 +0000

    images

--- source/main.js
+++ source/main.js
@@ -8,13 +8,21 @@ var showsbody = document.getElementById("shows");
 var i = 0
 for (i in thejson) {
     var p = document.createElement("p");
+    var img = document.createElement("img");
+
+    var ismovie = thejson[i].type.toLowerCase() == 'movie';
     
     p.innerHTML = thejson[i].title + ' - ' + thejson[i].release_date + (thejson[i].speculation == true ? ' (SPECULATION)' : ' (OFFICIAL)');
+    img.src = 'assets/pngs/'+(ismovie ? 'movies' : 'shows')+'/'+thejson[i].png_file_location
+    img.width = 100;
 
-    if (thejson[i].type.toLowerCase() == 'movie')
+    if (ismovie) {
         moviesbody.appendChild(p);
-    else
+        moviesbody.appendChild(img);
+    } else {
         showsbody.appendChild(p);
+        showsbody.appendChild(img);
+    }
 
     i++;
 }
